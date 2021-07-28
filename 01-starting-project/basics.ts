60ccfc1584f9af2b136a2482459355f0a62da587
yechan <noy3928@naver.com>
2021-07-28 19:46:16 +0900

    배열과 객체의 타입을 정하는 방법

--- 01-starting-project/basics.ts
+++ 01-starting-project/basics.ts
@@ -9,3 +9,26 @@ userName = 'Max';
 let isInstructor: boolean;
 
 isInstructor = true;
+
+//more complex types 
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+let person: {
+    name:string;
+    age:number;
+};
+
+person = {
+    name:'Max',
+    age:32
+}
+
+let people: {
+    name:string;
+    age:number;
+}[]
+
+//Type inference
