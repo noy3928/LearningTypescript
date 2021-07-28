54ee6d6a77d2fb736d9a28c2241e01d6f1bdeff7
yechan <noy3928@naver.com>
2021-07-28 19:53:19 +0900

    여러 타입을 동시에 사용하는 방법에 대해서 배웠다. | 특수문자를 사용하면, 가능하다.

--- 01-starting-project/basics.ts
+++ 01-starting-project/basics.ts
@@ -2,7 +2,8 @@ let age:number;
 
 age = 12;
 
-let userName:string;
+//문자열과 문자열배열 이 두가지 타입을 동시에 지정할 수 있다. 
+let userName:string | string[];
 
 userName = 'Max';
 
@@ -32,3 +33,6 @@ let people: {
 }[]
 
 //Type inference
+let course: string | number = 'React - The Complete Guide';
+
+course = 12341;
