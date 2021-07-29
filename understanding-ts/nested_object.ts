888fbd569bc33f6cf8a30df869a04057f5f230a0
yechan <noy3928@naver.com>
2021-07-29 19:34:03 +0900

    nested object

---
+++ understanding-ts/nested_object.ts
@@ -0,0 +1,20 @@
+const product = {
+    id: 'abc1',
+    price: 12.99,
+    tags: ['great-offer', 'hot-and-new'],
+    details: {
+      title: 'Red Carpet',
+      description: 'A great carpet - almost brand-new!'
+    }
+  }
+//위의 코드는 아래와 같은 타입으로 선언될 수 있다. 
+
+let products:{
+    id: string;
+    price: number;
+    tags: string[];
+    details: {
+      title: string;
+      description: string;
+    }
+  }
