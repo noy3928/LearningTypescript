ebf8e779696ebd4b3badb62f9b2147b9188a3396
yechan <noy3928@naver.com>
2021-07-29 19:32:08 +0900

    타입스크립트에서 객체를 선언하기

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,18 +1,10 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "Result is ...";
-add(number1, number2, printResult, resultPhrase);
-/*
-자바스크립트는 코드를 작성하는 중에 타입을 확인하지 않지만,
-타입스크립트는 계속해서 타입을 확인해준다.
-*/ 
+//타입스크립트에서 객체를 선언하는 방법 
+var person = {
+    name: 'Leo',
+    age: 30
+};
+// const person = {
+//     name: 'Leo',
+//     age:30,
+// }
+console.log(person.name);
---
+++ understanding-ts/app.ts
@@ -0,0 +1,15 @@
+//타입스크립트에서 객체를 선언하는 방법 
+const person:{
+    name:string;
+    age:number;
+} = {
+    name: 'Leo',
+    age:30,
+}
+
+// const person = {
+//     name: 'Leo',
+//     age:30,
+// }
+
+console.log(person.name)
