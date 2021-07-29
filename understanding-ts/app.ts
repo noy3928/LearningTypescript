a194a495cef1a65baa2497bca9263117a73aad03
yechan <noy3928@naver.com>
2021-07-30 03:40:49 +0900

    함수의 타입에 대하여

--- understanding-ts/app.ts
+++ understanding-ts/app.ts
@@ -2,3 +2,9 @@ function add(n1: number, n2: number){
     return n1 + n2;
 }
 
+//console.log같이 리턴하는 값이 없는 경우에는 void를 반환한다. 
+function printResult(num: number): void{
+    console.log('Result: ' + num)
+}
+console.log(printResult(add(5,12))) // 이렇게 출력하면 undefine이 나온다. 
+
--- understanding-ts/basics.ts
+++ understanding-ts/basics.ts
@@ -1,27 +1,27 @@
 
-function add(n1: number, n2:number, showResult: boolean, phrase: string){
+// function add(n1: number, n2:number, showResult: boolean, phrase: string){
 
-    //한 구문 안에서 더할 때, 스트링과 숫자가 같이 더해지면, 모두 문자열로 인식하게 된다. 
-    const result = n1 + n2;
-    if(showResult){
-        console.log(phrase + result)
-    }else{
-        return result;
-    }
+//     //한 구문 안에서 더할 때, 스트링과 숫자가 같이 더해지면, 모두 문자열로 인식하게 된다. 
+//     const result = n1 + n2;
+//     if(showResult){
+//         console.log(phrase + result)
+//     }else{
+//         return result;
+//     }
     
-}
+// }
 
-let number1: number;
-// number = "hi" //number로 선언해둔 타입에서는 문자열이 들어갈 수 없다. 
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is ..."
+// let number1: number;
+// // number = "hi" //number로 선언해둔 타입에서는 문자열이 들어갈 수 없다. 
+// const number2 = 2.8;
+// const printResult = true;
+// const resultPhrase = "Result is ..."
 
-add(number1, number2, printResult, resultPhrase);
+// add(number1, number2, printResult, resultPhrase);
 
 
 
-/*
-자바스크립트는 코드를 작성하는 중에 타입을 확인하지 않지만,
-타입스크립트는 계속해서 타입을 확인해준다. 
-*/
+// /*
+// 자바스크립트는 코드를 작성하는 중에 타입을 확인하지 않지만,
+// 타입스크립트는 계속해서 타입을 확인해준다. 
+// */
