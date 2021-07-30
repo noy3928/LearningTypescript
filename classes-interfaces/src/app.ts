0f1a1cbbea048fd092286892657761efd569a03a
yechan <noy3928@naver.com>
2021-07-30 21:57:55 +0900

    readonly에 대하여

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,12 +1,12 @@
 // Code goes here!
 class Department {
-    public name: string; //이것은 클래스의 필드라고 불린다. | public타입은 바깥에서도 이 값에 접근할 수 있게 한다. 
+    // public name: string; //이것은 클래스의 필드라고 불린다. | public타입은 바깥에서도 이 값에 접근할 수 있게 한다. 
     private employees:string[] = []; // private는 클래스 내부에서는 값을 사용할 수 있게 한다. 
 
-    //파라미터에서부터 private와 public을 선언할 수 있다. 
-    constructor(private id:string, public name : string){
-        this.name = n; 
-        this.id = id;
+    //파라미터에서부터 private와 public을 선언할 수 있다. | readonly는 값을 선언하고 난 이후에는 다시 값을 변경할 수 없다. 
+    constructor(private readonly id:string, public name : string){
+        // this.name = n; 
+        // this.id = id;
     }
 
     describe(this: Department) {
