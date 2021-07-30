f46ede915933ef84d9163b6786f19d39492cb827
yechan <noy3928@naver.com>
2021-07-30 21:52:33 +0900

    public과 private에 대하여

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,20 +1,41 @@
 // Code goes here!
 class Department {
-    name: string; //이것은 클래스의 필드라고 불린다. 
+    public name: string; //이것은 클래스의 필드라고 불린다. | public타입은 바깥에서도 이 값에 접근할 수 있게 한다. 
+    private employees:string[] = []; // private는 클래스 내부에서는 값을 사용할 수 있게 한다. 
 
-    constructor(n : string){
+    //파라미터에서부터 private와 public을 선언할 수 있다. 
+    constructor(private id:string, public name : string){
         this.name = n; 
+        this.id = id;
     }
 
     describe(this: Department) {
-        console.log('Department: ' + this.name );
+        console.log(`Department(${this.id}): ${this.name}  `+ this.name );
     }
 
+    addEmployee(employee: string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees)
+    }
 }
 
 const accounting = new Department('Accounting');
 
-accounting.describe();
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu')
 
-const accountingCopy = {name: 's', describe: accounting.describe};
-accountingCopy.describe() //describe의 this에 Department 타입을 넣으면 인스턴스를 생성할 때에도, 그 클래스의 형식을 맞춰줘야한다. 
+// accounting.employees[2] = 'Anna'; //private으로 등록하기 전까지는 밖에서 값을 넣을 수 잇었지만, private 이후에는 밖에서 값을 등록할 수 없어진다. 
+accounting.name = 'NEW'//이것은 퍼블릭 타입에 의해서 가능해 진 것이다. 사실은 이것이 기본값이기도 하다. 
+
+accounting.describe();
+accounting.printEmployeeInformation();
+
+
+
+// const accountingCopy = {name: 's', describe: accounting.describe};
+// accountingCopy.describe() //describe의 this에 Department 타입을 넣으면 인스턴스를 생성할 때에도, 그 클래스의 형식을 맞춰줘야한다. 
+
